33013a1c374c4f3c47cd6aae9463c601fc8b9a76
Mohsin-Naqvi <naqvi645@gmail.com>
2022-03-28 01:06:09 +0500

    Product Recuder
    FETCH_ALL reducer changes .

--- src/reducers/Product.js
+++ src/reducers/Product.js
@@ -13,7 +13,6 @@ export default (state = initialState, action) => {
         _TotalRecords = action.payload.TotalRecord;
         console.log('data: ', data)
         return {
-          ...state,
           ProductList: data,
           TotalRecords: _TotalRecords
         };
